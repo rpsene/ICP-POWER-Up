05f2b046012be5416480f6a8a65eccb07ccc45e1
Mick Tarsel <mtarsel@gmail.com>
2018-07-31 11:23:49 -0700

    adding semicolons

--- install-icp-ppc64le.sh
+++ install-icp-ppc64le.sh
@@ -3,10 +3,10 @@
 #Written by Mick Tarsel
 
 gen_ssh_keys(){
-  ssh-keygen -b 4096 -f ~/.ssh/id_rsa -N ""
-  cat ~/.ssh/id_rsa.pub | sudo tee -a ~/.ssh/authorized_keys
+  ssh-keygen -b 4096 -f ~/.ssh/id_rsa -N "";
+  cat ~/.ssh/id_rsa.pub | sudo tee -a ~/.ssh/authorized_keys;
   #ssh-copy-id -i ~/.ssh/id_rsa.pub <user>@<node_ip_address>
-  sudo systemctl restart sshd
+  sudo systemctl restart sshd;
 }
 
 apt-get update
